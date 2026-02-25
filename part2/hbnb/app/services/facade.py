316cac5db6ee0aafd805c88879cd702caf77c05d
Ali731-Amb <12252@holbertonstudents.com>
2026-02-25 23:05:11 +0100

    upadate_facade_add_pets_and_others

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -14,12 +14,26 @@ class HBnBFacade:
         user = User(**user_data)
         self.user_repo.add(user)
         return user
+    
+    def update_user(self, user_id, user_data):
+        user = self.user_repo.get(user_id)
+        if user_id is None:
+            return None
+        for key, value in user_data.items():
+            setattr(user, key, value)
+        return user
 
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
 
     def get_user_by_email(self, email):
         return self.user_repo.get_by_attribute('email', email)
+    
+    def get_all_users(self):
+        return self.user_repo.get_all()
+    
+    def delete_user(self, user_id):
+        return self.user_repo.delete(user_id)
 
 #-------------------- Place --------------------------
 
