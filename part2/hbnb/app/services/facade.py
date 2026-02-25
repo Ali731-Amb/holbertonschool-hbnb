d9211eb8bd12d524159240fd25091dfd7cdb64f3
Ali731-Amb <12252@holbertonstudents.com>
2026-02-25 23:27:42 +0100

    Update_facade_amenity

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 class HBnBFacade:
     def __init__(self):
@@ -41,3 +42,22 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+#---------------------Amenity-------------------------
+def create_amenity(self, amenity_data):
+    amenity = Amenity(**amenity_data)
+    self.amenity_repo.add(amenity)
+    return amenity
+
+def get_amenity(self, amenity_id):
+    return self.amenity_repo.get(amenity_id)
+
+def get_all_amenities(self):
+    return self.amenity_repo.get_all()
+
+def update_amenity(self, amenity_id, amenity_data):
+    amenity = self.amenity_repo.get(amenity_id)
+    if amenity:
+        for key, value in amenity_data.items():
+            setattr(amenity, key, value)
+    return amenity
