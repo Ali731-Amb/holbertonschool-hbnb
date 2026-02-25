f07e2509babf33e2b67968d38d7d4b5007329d59
Ali731-Amb <12252@holbertonstudents.com>
2026-02-25 23:03:07 +0100

    users_API_implemented

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -5,13 +5,27 @@ api = Namespace('users', description='User operations')
 
 # Define the user model for input validation and documentation
 user_model = api.model('User', {
+    'id': fields.String(readOnly=True, description='The unique identifier of the user'),
     'first_name': fields.String(required=True, description='First name of the user'),
     'last_name': fields.String(required=True, description='Last name of the user'),
-    'email': fields.String(required=True, description='Email of the user')
-})
+    'email': fields.String(required=True, description='Email of the user'),
+    'pets': fields.String(description='Pet of the user', enum=['CHIEN', 'CHAT', 'OTHERS'])
+    })
 
 @api.route('/')
 class UserList(Resource):
+    @api.response(200, 'Success')
+    def get(self):
+        """List all users"""
+        users = facade.get_all_users()
+        return [{
+        'id': u.id,
+        'first_name': u.first_name,
+        'last_name': u.last_name,
+        'email': u.email,
+        'pets': u.pets.name if u.pets else None
+                } for u in users], 200
+    
     @api.expect(user_model, validate=True)
     @api.response(201, 'User successfully created')
     @api.response(400, 'Email already registered')
@@ -26,4 +40,57 @@ class UserList(Resource):
             return {'error': 'Email already registered'}, 400
 
         new_user = facade.create_user(user_data)
-        return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
+        return {
+            'id': new_user.id, 
+            'first_name': new_user.first_name, 
+            'last_name': new_user.last_name, 
+            'email': new_user.email,
+            'pets': new_user.pets.name if new_user.pets else None
+                } , 201
+    
+
+@api.route('/<user_id>')
+class UserResource(Resource):
+    @api.response(200, 'User details retrieved successfully')
+    @api.response(404, 'User not found')
+    def get(self, user_id):
+        """Get user details by ID"""
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        return {
+            'id': user.id, 
+            'first_name': user.first_name, 
+            'last_name': user.last_name, 
+            'email': user.email,
+            'pets': user.pets.name if user.pets else None
+                }, 200
+    
+    @api.expect(user_model, validate = True)
+    @api.response(200, 'User successfully upadated')
+    @api.response(404, 'User not found')
+    @api.response(400, 'Invalid input data')
+    def put(self, user_id):
+        user_data = api.payload
+        if 'email' in user_data:
+            existing_user = facade.get_user_by_email(user_data['email'])
+            if existing_user and existing_user.id != user_id:
+                return {'error' : 'Email is already registered'}, 400
+        updated_user = facade.update_user(user_id, user_data)
+        if updated_user is None: 
+            return {'error' : 'User not found'}, 404
+        return{
+            'id': updated_user.id, 
+            'first_name': updated_user.first_name, 
+            'last_name': updated_user.last_name, 
+            'email': updated_user.email,
+            'pets': updated_user.pets.name if updated_user.pets else None
+                } , 200
+
+    @api.response(200, 'User successfully deleted')
+    @api.response(400, 'Invalid input data')
+    def delete(self, user_id):
+        delete_user = facade.delete_user(user_id)
+        if delete_user is False: 
+            return {'error' : 'User not found'}, 404
+        return {'message' : 'User deleted successfully'}, 200
