60d0db3864840c798337d3ac3180a38e4b9303f6
Ali731-Amb <12252@holbertonstudents.com>
2026-02-25 02:36:51 +0100

    Class_Place

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,7 +1,8 @@
 from .base_model import BaseModel
+from .user import User
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
+    def __init__(self, title, price, latitude, longitude, owner, description=None):
         super().__init__()
         self.title = title
         self.description = description
@@ -12,24 +13,100 @@ class Place(BaseModel):
         self.reviews = []  # List to store related reviews
         self.amenities = []  # List to store related amenities
 
+
+#----------------- Title -----------------
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Title must be a string of characters.")
+        if len(value) > 100:
+            raise ValueError("Title must be under 100 characters")
+        self._title = value
+
+#-----------------Description ------------
+    @property
+    def description(self):
+        return self._description
+    
+    @description.setter
+    def description(self, value):
+        if value is not None and not isinstance(value, str):
+            raise ValueError("Description must be a string or None")
+        self._description = value
+#------------------Price -----------------
+    @property
+    def price(self):
+        return self._price
+    
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError("The price must be an integer or a decimal number")
+        if value <= 0:
+            raise ValueError("The price must be greater than 0.")
+        self._price = value
+
+#-----------------Latitude ---------------
+    @property
+    def latitude(self):
+        return self._latitude 
+    
+    @latitude.setter
+    def latitude(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError("Latitude must be an integer or a decimal number")
+        if value < -90.0  or value > 90.0 :
+            raise ValueError("The latitude must be between -90.0 and 90.0")
+        self._latitude = value
+
+#----------------Longitude----------------
+    @property
+    def longitude(self):
+        return self._longitude 
+    
+    @longitude.setter 
+    def longitude(self, value):
+        if not isinstance(value, (float, int)):
+            raise ValueError("Longitude must be an integer or a decimal number")
+        if value < -180.0 or value > 180.0:
+            raise ValueError("The longitude must be between -180.0 and 180")
+        self._longitude = value
+#----------------Owner--------------------
+    @property
+    def owner(self):
+        return self._owner
+    
+    @owner.setter
+    def owner(self, value):
+        if not isinstance(value, User):
+            raise TypeError("The owner must belong to the user class")
+        self._owner = value
+#----------------Review------------------
     def add_review(self, review):
         """Add a review to the place."""
         self.reviews.append(review)
 
+#----------------Amenity------------------
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
         if amenity not in self.amenities:
             self.amenities.append(amenity)
 
-def to_dict(self):
-    return {
+
+#---------------------- Dictionnaire ----------------------
+    def to_dict(self):
+        return {
             "id": self.id,
             "title": self.title,
             "description": self.description,
             "price": self.price,
             "latitude": self.latitude,
             "longitude": self.longitude,
-            "owner.id": self.owner/id if self.owner else None,
+            "owner_id": self.owner.id if self.owner else None,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat()
-        }
+            }
