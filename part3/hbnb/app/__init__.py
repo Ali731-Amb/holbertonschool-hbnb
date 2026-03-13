ba4e0292c797cd391d8de95e0d8e1f6d95c62d1d
Ali731-Amb <12252@holbertonstudents.com>
2026-03-13 14:15:48 +0100

    add_jwt_requirements

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,13 +1,24 @@
+import os
+import config
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
 
-def create_app():
+def create_app(config_class=config.DevelopmentConfig):
         app = Flask(__name__)
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///hbnb.db')
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        db.init_app(app)
 
         # Configuration de l'API (Swagger)
         api = Api(app, 
@@ -22,4 +33,5 @@ def create_app():
         api.add_namespace(places_ns, path='/api/v1/places')
         api.add_namespace(reviews_ns, path='/api/v1/reviews')
 
+        bcrypt.init_app(app)
         return app
