bfb186b768eeb6bb96faf6692015db87758f85fb
Ali731-Amb <12252@holbertonstudents.com>
2026-03-28 16:39:53 +0100

    deletd_comment_admin_api

--- part3/hbnb/app/api/v1/auth.py
+++ part3/hbnb/app/api/v1/auth.py
@@ -5,7 +5,6 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Namespace('auth', description='Authentication operations')
 
-# Model for input validation
 login_model = api.model('Login', {
     'email': fields.String(required=True, description='User email'),
     'password': fields.String(required=True, description='User password')
@@ -16,18 +15,14 @@ class Login(Resource):
     @api.expect(login_model)
     def post(self):
         """Authenticate user and return a JWT token"""
-        credentials = api.payload  # Get the email and password from the request payload
-        # Step 1: Retrieve the user based on the provided email
+        credentials = api.payload
         user = facade.get_user_by_email(credentials['email'])
-        # Step 2: Check if the user exists and the password is correct
         if not user or not user.verify_password(credentials['password']):
             return {'error': 'Invalid credentials'}, 401
-        # Step 3: Create a JWT token with the user's id and is_admin flag
         access_token = create_access_token(
-        identity=str(user.id),   # only user ID goes here
-        additional_claims={"is_admin": user.is_admin}  # extra info here
+        identity=str(user.id),
+        additional_claims={"is_admin": user.is_admin}
         )
-        # Step 4: Return the JWT token to the client
         return {'access_token': access_token}, 200
 
 @api.route('/protected')
@@ -37,8 +32,5 @@ class ProtectedResource(Resource):
         """A protected endpoint that requires a valid JWT token"""
         print("jwt------")
         print(get_jwt_identity())
-        current_user = get_jwt_identity() # Retrieve the user's identity from the token
-        #if you need to see if the user is an admin or not, you can access additional claims using get_jwt() :
-        # addtional claims = get_jwt()
-        #additional claims["is_admin"] -> True or False
+        current_user = get_jwt_identity()
         return {'message': f'Hello, user {current_user}'}, 200
