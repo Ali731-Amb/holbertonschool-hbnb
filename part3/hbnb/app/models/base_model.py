1517f2df23814d2c6463ab7082806fe2b869ee94
Ali731-Amb <12252@holbertonstudents.com>
2026-03-29 03:32:34 +0200

    update_base_model_mapping+super_init_kwargs

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -3,13 +3,17 @@ import uuid
 from datetime import datetime
 
 class BaseModel(db.Model):
-    __abstract__ = True
+    __abstract__ = True  # SQLAlchemy ne crée pas de table pour cette classe
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        # Init minimal pour permettre héritage et compatibilité ORM
+        self.id = kwargs.get('id', str(uuid.uuid4()))
+        self.created_at = kwargs.get('created_at', datetime.utcnow())
+        self.updated_at = kwargs.get('updated_at', datetime.utcnow())
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
