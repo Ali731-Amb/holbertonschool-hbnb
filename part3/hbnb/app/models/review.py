26dabc7e2607776365e67bc7c8804893f3575b66
Ali731-Amb <12252@holbertonstudents.com>
2026-03-29 03:37:20 +0200

    update_review__mapping+super_init_kwargs

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -5,14 +5,17 @@ from .base_model import BaseModel
 class Review(BaseModel):
     __tablename__ = 'reviews'
 
-    text = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.String(36), db.ForeignKey(
-        'users.id'), nullable=False)
-    place_id = db.Column(db.String(36), db.ForeignKey(
-        'places.id'), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        self.content = kwargs.get('content')
+        self.rating = kwargs.get('rating')
+        self.user_id = kwargs.get('user_id')
+        self.place_id = kwargs.get('place_id')
 
 # --------------- Text -------------------
 
