8f04c990ad7ea79c39f8ed44219d726af77f44f9
Ali731-Amb <12252@holbertonstudents.com>
2026-03-29 03:09:37 +0200

    update_amenity_mapping

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -1,29 +1,34 @@
+from app import db
 from .base_model import BaseModel
 
+
 class Amenity(BaseModel):
-	def __init__(self, name, **kwargs):
-		super().__init__(**kwargs)
-		self.name = name
+    __tablename__ = 'amenities'
 
-	@property
-	def name(self):
-		return self._name
-	
-	@name.setter
-	def name(self, value):
-		if not isinstance(value, str):
-			raise ValueError("Amenity name must be a string")
-		if not value.strip(): 
-			raise ValueError("Amenity name cannot be empty")
-		if len(value) > 50:
-			raise ValueError("Amenity name must be under 50 characters")
-		self._name = value
+    name = db.Column(db.String(100), nullable=False, unique=True)
 
-#------------------------Dictionnaire-------------------------------
-	def to_dict(self):
-		return {
-			"id": self.id,
-			"name": self.name,
-			"created_at": self.created_at.isoformat(),
-			"updated_at": self.updated_at.isoformat()
-				}
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        @property
+        def name(self):
+            return self._name
+
+        @name.setter
+        def name(self, value):
+            if not isinstance(value, str):
+                raise ValueError("Amenity name must be a string")
+            if not value.strip():
+                raise ValueError("Amenity name cannot be empty")
+            if len(value) > 50:
+                raise ValueError("Amenity name must be under 50 characters")
+            self._name = value
+
+# ------------------------Dictionnaire-------------------------------
+        def to_dict(self):
+            return {
+                "id": self.id,
+                "name": self.name,
+                "created_at": self.created_at.isoformat(),
+                "updated_at": self.updated_at.isoformat()
+            }
