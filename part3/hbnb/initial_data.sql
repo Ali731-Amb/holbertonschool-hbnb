48a2488c9c4581e5d6d1a00c86e937eef4c3ad97
Ali731-Amb <12252@holbertonstudents.com>
2026-04-01 01:16:17 +0200

    change_hash_password_initial_data

--- part3/hbnb/initial_data.sql
+++ part3/hbnb/initial_data.sql
@@ -1,24 +1,26 @@
+-- Nettoyage avant insertion
+DELETE FROM place_amenity;
+DELETE FROM reviews;
+DELETE FROM places;
+DELETE FROM amenities;
+DELETE FROM users;
+
 -- =====================
 -- Utilisateur Admin
 -- =====================
-DELETE FROM Place_Amenity;
-DELETE FROM Review;
-DELETE FROM Place;
-DELETE FROM Amenity;
-DELETE FROM users;
 INSERT INTO users (id, first_name, last_name, email, password_hash, is_admin)
 VALUES (
     '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
     'Admin',
     'HBnB',
     'admin@hbnb.io',
-    '$2b$12$TKJDoP1SU0XvHXtQX.UDGeFoDYxNcy8ibONHnA6Rm1X4OPv2EzZj6',
+    '$2b$12$NVkYW0OzyiII3/d8k77Z2.yYogb6NGQ9nQ.ueTEnVrEQAU4fvwupu',
     TRUE
 );
 
 -- =====================
 -- Amenities
 -- =====================
-INSERT INTO Amenity (id, name) VALUES ('467073a3-b3c0-4c1f-9831-5467a67dcea5',          'WiFi');
-INSERT INTO Amenity (id, name) VALUES ('160cb549-80bc-4f3d-9184-5bd98a914c24',       'Piscine');
-INSERT INTO Amenity (id, name) VALUES ('b6b08f7c-e8c0-48fd-9acf-d265d19fe09d', 'Climatisation');
+INSERT INTO amenities (id, name) VALUES ('467073a3-b3c0-4c1f-9831-5467a67dcea5', 'WiFi');
+INSERT INTO amenities (id, name) VALUES ('160cb549-80bc-4f3d-9184-5bd98a914c24', 'Piscine');
+INSERT INTO amenities (id, name) VALUES ('b6b08f7c-e8c0-48fd-9acf-d265d19fe09d', 'Climatisation');
