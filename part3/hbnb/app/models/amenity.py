ce7a463ed9c63ecd03614a34bfa86025741307b1
Ali731-Amb <12252@holbertonstudents.com>
2026-03-31 17:22:46 +0200

    change_indentation_model_amenity

--- part3/hbnb/app/models/amenity.py
+++ part3/hbnb/app/models/amenity.py
@@ -13,25 +13,25 @@ class Amenity(BaseModel):
         self.name = kwargs.get('name')
         self.description = kwargs.get('description')
 
-        @property
-        def name(self):
-            return self._name
+    @property
+    def name(self):
+        return self._name
 
-        @name.setter
-        def name(self, value):
-            if not isinstance(value, str):
-                raise ValueError("Amenity name must be a string")
-            if not value.strip():
-                raise ValueError("Amenity name cannot be empty")
-            if len(value) > 50:
-                raise ValueError("Amenity name must be under 50 characters")
-            self._name = value
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Amenity name must be a string")
+        if not value.strip():
+            raise ValueError("Amenity name cannot be empty")
+        if len(value) > 50:
+            raise ValueError("Amenity name must be under 50 characters")
+        self._name = value
 
 # ------------------------Dictionnaire-------------------------------
-        def to_dict(self):
-            return {
-                "id": self.id,
-                "name": self.name,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
-            }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
