bd1dea1bae3dc41981e6265ffb935953c65af733
Ali731-Amb <12252@holbertonstudents.com>
2026-04-01 01:07:34 +0200

    change_users_en_user_schema_sql

--- part3/hbnb/schema.sql
+++ part3/hbnb/schema.sql
@@ -8,13 +8,13 @@ DROP TABLE IF EXISTS User;
 -- =====================
 -- TABLE : User
 -- =====================
-CREATE TABLE User (
-    id         CHAR(36)     PRIMARY KEY,
-    first_name VARCHAR(255),
-    last_name  VARCHAR(255),
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    is_admin   BOOLEAN      DEFAULT FALSE
+CREATE TABLE users (
+    id           CHAR(36)     PRIMARY KEY,
+    first_name   VARCHAR(50)  NOT NULL,
+    last_name    VARCHAR(50)  NOT NULL,
+    email        VARCHAR(120) UNIQUE NOT NULL,
+    password_hash VARCHAR(128) NOT NULL,
+    is_admin     BOOLEAN      DEFAULT FALSE
 );
 
 -- =====================
