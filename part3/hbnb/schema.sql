7e9cd15d272f89cd926ee4ffbd6b31ed9510ecab
Ali731-Amb <12252@holbertonstudents.com>
2026-04-01 01:34:30 +0200

    add_created_update_schema_sql

--- part3/hbnb/schema.sql
+++ part3/hbnb/schema.sql
@@ -1,4 +1,4 @@
--- Supprime les tables dans le bon ordre (enfants avant parents)
+-- Supprime les tables dans le bon ordre
 DROP TABLE IF EXISTS place_amenity;
 DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS places;
@@ -15,7 +15,9 @@ CREATE TABLE users (
     email         VARCHAR(120) UNIQUE NOT NULL,
     password_hash VARCHAR(128) NOT NULL,
     is_admin      BOOLEAN      DEFAULT FALSE,
-    pet           VARCHAR(20)
+    pet           VARCHAR(20),
+    created_at    DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    updated_at    DATETIME     DEFAULT CURRENT_TIMESTAMP
 );
 
 -- =====================
@@ -29,6 +31,8 @@ CREATE TABLE places (
     latitude    FLOAT,
     longitude   FLOAT,
     owner_id    CHAR(36)       NOT NULL,
+    created_at  DATETIME       DEFAULT CURRENT_TIMESTAMP,
+    updated_at  DATETIME       DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
@@ -36,11 +40,13 @@ CREATE TABLE places (
 -- TABLE : reviews
 -- =====================
 CREATE TABLE reviews (
-    id       CHAR(36) PRIMARY KEY,
-    text     TEXT,
-    rating   INT CHECK (rating >= 1 AND rating <= 5),
-    user_id  CHAR(36) NOT NULL,
-    place_id CHAR(36) NOT NULL,
+    id         CHAR(36) PRIMARY KEY,
+    text       TEXT,
+    rating     INT CHECK (rating >= 1 AND rating <= 5),
+    user_id    CHAR(36) NOT NULL,
+    place_id   CHAR(36) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id)  REFERENCES users(id)  ON DELETE CASCADE,
     FOREIGN KEY (place_id) REFERENCES places(id) ON DELETE CASCADE,
     UNIQUE (user_id, place_id)
@@ -50,8 +56,10 @@ CREATE TABLE reviews (
 -- TABLE : amenities
 -- =====================
 CREATE TABLE amenities (
-    id   CHAR(36)     PRIMARY KEY,
-    name VARCHAR(255) UNIQUE NOT NULL
+    id         CHAR(36)     PRIMARY KEY,
+    name       VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATETIME     DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME     DEFAULT CURRENT_TIMESTAMP
 );
 
 -- =====================
