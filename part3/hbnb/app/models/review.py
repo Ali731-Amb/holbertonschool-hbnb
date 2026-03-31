3f669705775b3663182eb4be105c6287cc9dddd0
Ali731-Amb <12252@holbertonstudents.com>
2026-03-31 17:21:36 +0200

    change_indentation_model_review

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -22,63 +22,62 @@ class Review(BaseModel):
 
 # --------------- Text -------------------
 
-        @property
-        def text(self):
-            return self._text
+    @property
+    def text(self):
+        return self._text
 
-        @text.setter
-        def text(self, value):
-            if not isinstance(value, str):
-                raise ValueError("Text must be a string of characters")
-            if len(value) < 10:
-                raise ValueError("Text must be longer than 10 characters")
-            self._text = value
+    @text.setter
+    def text(self, value):
+        if not isinstance(value, str):
+            raise ValueError("Text must be a string of characters")
+        if len(value) < 10:
+            raise ValueError("Text must be longer than 10 characters")
+        self._text = value
 
 # --------------- Rating -----------------
-        @property
-        def rating(self):
-            return self._rating
+    @property
+    def rating(self):
+        return self._rating
 
-        @rating.setter
-        def rating(self, value):
-            if not isinstance(value, int):
-                raise ValueError("Rating must be an interger")
-            if not 1 <= value <= 5:
-                raise ValueError("The grade must be between 1 and 5. ")
-            self._rating = value
-
+    @rating.setter
+    def rating(self, value):
+        if not isinstance(value, int):
+            raise ValueError("Rating must be an interger")
+        if not 1 <= value <= 5:
+            raise ValueError("The grade must be between 1 and 5. ")
+        self._rating = value
 
 # --------------- Place ------------------
 
-        @property
-        def place(self):
-            return self._place
+    @property
+    def place(self):
+        return self._place
 
-        @place.setter
-        def place(self, value):
-            if type(value).__name__ != 'Place':
-                raise ValueError("Place must be an instance of Place class")
-            self._place = value
+    @place.setter
+    def place(self, value):
+        if type(value).__name__ != 'Place':
+            raise ValueError("Place must be an instance of Place class")
+        self._place = value
 
 # --------------- User -------------------
-        @property
-        def user(self):
-            return self._user
+    @property
+    def user(self):
+        return self._user
 
-        @user.setter
-        def user(self, value):
-            if type(value).__name__ != 'User':
-                raise ValueError("User must be an instance of User class")
-            self._user = value
+    @user.setter
+    def user(self, value):
+        if type(value).__name__ != 'User':
+            raise ValueError("User must be an instance of User class")
+        self._user = value
 
 # ------------------Dictionnaire--------------------
-        def to_dict(self):
-            return {
-                "id": self.id,
-                "text": self.text,
-                "rating": self.rating,
-                "place_id": self.place.id,
-                "user_id": self.user.id,
-                "created_at": self.created_at.isoformat(),
-                "updated_at": self.updated_at.isoformat()
-            }
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "text": self.text,
+            "rating": self.rating,
+            "place_id": self.place.id,
+            "user_id": self.user.id,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
