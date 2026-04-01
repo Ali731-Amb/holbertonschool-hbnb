845f89c017e6f50fe89acbb2eeac06aa4d1769fe
Ali731-Amb <12252@holbertonstudents.com>
2026-04-01 03:36:20 +0200

    change_review_model_init

--- part3/hbnb/app/models/review.py
+++ part3/hbnb/app/models/review.py
@@ -3,81 +3,27 @@ from .base_model import BaseModel
 
 
 class Review(BaseModel):
-    __tablename__ = 'reviews'
+    __tablename__ = "reviews"
 
     text = db.Column(db.String(500), nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
-
-    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
-    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey("users.id"), nullable=False)
+    place_id = db.Column(db.String(36), db.ForeignKey("places.id"), nullable=False)
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.text = kwargs.get('text')
-        self.rating = kwargs.get('rating')
-        self.user_id = kwargs.get('user_id')
-        self.place_id = kwargs.get('place_id')
+        self.text = kwargs.get("text")
+        self.rating = kwargs.get("rating")
+        self.user_id = kwargs.get("user_id")
+        self.place_id = kwargs.get("place_id")
 
-# --------------- Text -------------------
-
-    @property
-    def text(self):
-        return self._text
-
-    @text.setter
-    def text(self, value):
-        if not isinstance(value, str):
-            raise ValueError("Text must be a string of characters")
-        if len(value) < 10:
-            raise ValueError("Text must be longer than 10 characters")
-        self._text = value
-
-# --------------- Rating -----------------
-    @property
-    def rating(self):
-        return self._rating
-
-    @rating.setter
-    def rating(self, value):
-        if not isinstance(value, int):
-            raise ValueError("Rating must be an interger")
-        if not 1 <= value <= 5:
-            raise ValueError("The grade must be between 1 and 5. ")
-        self._rating = value
-
-# --------------- Place ------------------
-
-    @property
-    def place(self):
-        return self._place
-
-    @place.setter
-    def place(self, value):
-        if type(value).__name__ != 'Place':
-            raise ValueError("Place must be an instance of Place class")
-        self._place = value
-
-# --------------- User -------------------
-    @property
-    def user(self):
-        return self._user
-
-    @user.setter
-    def user(self, value):
-        if type(value).__name__ != 'User':
-            raise ValueError("User must be an instance of User class")
-        self._user = value
-
-# ------------------Dictionnaire--------------------
     def to_dict(self):
         return {
             "id": self.id,
             "text": self.text,
             "rating": self.rating,
-            "place_id": self.place.id,
-            "user_id": self.user.id,
+            "place_id": self.place_id,
+            "user_id": self.user_id,
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat()
         }
