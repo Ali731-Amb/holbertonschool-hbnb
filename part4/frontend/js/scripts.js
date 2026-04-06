6a060080f4c1e374d50e24c2812d36faa3fd5be3
Ali731-Amb <12252@holbertonstudents.com>
2026-04-06 16:03:23 +0200

    add_place_card_css_update_just_card_script_js_update_add_const_place

--- part4/frontend/js/scripts.js
+++ part4/frontend/js/scripts.js
@@ -6,3 +6,11 @@ Please, follow the project instructions to complete the tasks.
 document.addEventListener('DOMContentLoaded', () => {
     /* DO SOMETHING */
 });
+
+/* Place en dur (à changer) */ 
+
+const places = [
+    { name: "Beach House", price: 120, location: "Miami, FL", amenities: "Pool, Wi-Fi, Parking" },
+    { name: "Mountain Cabin", price: 90, location: "Aspen, CO", amenities: "Fireplace, Hot Tub" },
+    { name: "City Apartment", price: 75, location: "New York, NY", amenities: "Wi-Fi, Elevator" }
+];
